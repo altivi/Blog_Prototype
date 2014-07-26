d77942c1f36f72010fb0ab051acfd5b87ebe1861
Altivi <altivi.prog@gmail.com>
2014-07-26 11:05:42 +0200

    The End Two

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,6 @@ class UsersController < ApplicationController
 	def create
 		@user = User.new(user_params)
 		if @user.save
-			debugger
 			session[:user_id] = @user.id
 			redirect_to root_url
 		else
