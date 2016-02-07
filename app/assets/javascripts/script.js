33408ba6277b74e6c046c5c287bdc19a8a74d58c
Altivi <altivi.prog@gmail.com>
2016-02-08 00:44:25 +0200

    Added dynamic updating of comments

---
+++ app/assets/javascripts/script.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+
+	getComments();
+	var commentsQueryTimer = setInterval(function(){ //Parse comments every 10sec
+		getComments();
+	}, 1000);
+
+
+});
+
+var comments = new Array();
+var adressPOST = window.location.pathname;
+var adressGET = adressPOST + '.json';
+
+function getComments(){
+	$.getJSON(adressGET, function(data){
+		fillArray(data);
+	});
+}
+
+function fillArray(data){
+	var newComments = 0;
+	for(var i = 0; i < data.length; i++){
+		var found = false;
+		for(var j = 0; j < comments.length; j++){
+			if(comments[j].id == data[i].id){
+				found = true;
+				break;
+			}
+		}
+		if(found || data[i].id === undefined) continue;
+		newComments++;
+		comments.push(data[i]);
+	}
+	fillComments(newComments);
+}
+
+function fillComments(newComments){
+	for(var i = comments.length - newComments; i < comments.length; i++){
+		var post = $('<div class="post"></div>');
+		post.html('<h4><small>' + comments[i].email + ' says:' + '</small></h4><div class="well well-sm">' + comments[i].text + '</div');
+		$('#comments').append(post);
+	}
+}
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,6 +9,11 @@ class PostsController < ApplicationController
 		@post = Post.find(params[:id])
 		@comments = @post.comments
 		@new_comment = Comment.new(post: @post)
+		respond_to do |format|
+			format.html
+			format.json { render :json => comments_to_json_hash(@comments) }
+			format.js
+		end
 	end
 
 	# .select(:id, :text, :user_id)
@@ -38,6 +43,18 @@ class PostsController < ApplicationController
 
 	def post_params
 		params.require(:post).permit(:content, :title)
+	end
+
+	def comments_to_json_hash(comments)
+		@array = Array.new
+		comments.each do |c|
+			@hash = Hash.new
+			@hash[:id] = c.id
+			@hash[:text] = c.text
+			@hash[:email] = User.find(c[:user_id]).email
+			@array << @hash
+		end
+		return @array
 	end
 
 	# def search_params
