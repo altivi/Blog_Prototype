36727b57e5fa652c4cbff63db1821e5895630ad7
OneOfCats <krableshtraus@gmail.com>
2016-02-08 21:59:10 +0200

    comment fix

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -48,11 +48,11 @@ function fillArray(data){
 }
 
 function fillComments(newComments){
-	for(var i = comments.length - 1; i >= comments.length - newComments; i--){
+	for(var i = comments.length - newComments; i < comments.length ; i++){
 		var post = $('<div class="post"></div>');
 		post.css('display', 'none');
 		post.html('<h4><small>' + comments[i].user.email + ' says:' + '</small></h4><div class="well well-sm">' + comments[i].text + '</div');
-		$('#comments').append(post);
+		$('#comments').prepend(post);
 		post.fadeIn();
 	}
 }
