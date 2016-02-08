c170087f038dd31adcde4d735b05b9f444dbb109
OneOfCats <krableshtraus@gmail.com>
2016-02-08 13:15:04 +0200

    turbolinks and scroll fixes

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -1,16 +1,27 @@
-$(document).ready(function(){
+function initPage(){
+	
+	$('#sendComment').click(function(event){
+		event.preventDefault();
+		sendComment();
+	});
 
 	getComments();
 	var commentsQueryTimer = setInterval(function(){ //Parse comments every 10sec
 		getComments();
-	}, 1000);
+	}, 10000);
 
+}
 
+$(function(){
+	initPage();
+});
+$(window).bind('page:change', function(){
+	initPage();
 });
 
-var comments = new Array();
+var allComments = new Array();
 var adressPOST = window.location.pathname;
-var adressGET = adressPOST + '.json';
+var adressGET = adressPOST + '/json';
 
 function getComments(){
 	$.getJSON(adressGET, function(data){
@@ -22,23 +33,43 @@ function fillArray(data){
 	var newComments = 0;
 	for(var i = 0; i < data.length; i++){
 		var found = false;
-		for(var j = 0; j < comments.length; j++){
-			if(comments[j].id == data[i].id){
+		for(var j = 0; j < allComments.length; j++){
+			if(allComments[j].id == data[i].id){
 				found = true;
 				break;
 			}
 		}
 		if(found || data[i].id === undefined) continue;
 		newComments++;
-		comments.push(data[i]);
+		allComments.push(data[i]);
 	}
 	fillComments(newComments);
 }
 
 function fillComments(newComments){
-	for(var i = comments.length - newComments; i < comments.length; i++){
+	for(var i = allComments.length - newComments; i < allComments.length; i++){
 		var post = $('<div class="post"></div>');
-		post.html('<h4><small>' + comments[i].user.email + ' says:' + '</small></h4><div class="well well-sm">' + comments[i].text + '</div');
-		$('#comments').append(post);
+		post.html('<h4><small>' + allComments[i].email + ' says: id:' + allComments[i].id + '</small></h4><div class="well well-sm">' + allComments[i].text + '</div');
+		$('#allComments').append(post);
 	}
+}
+
+function sendComment(){
+	var text = $('#comment_area').val();
+	if(text.length == 0){
+		return;
+	}
+	$('#comment_area').val('');
+	var userId = $.cookie('user_id');
+	if(userId.length == 0){
+		alert('Session error: can\'t get user_id');
+		return;
+	}
+	var send = {user_id: userId, text: text};
+	$.post(adressPOST, $toJSON(send), function(data){
+		setTimeout(function(){
+			getComments();
+		}, 300);
+	});
+	$('html, body').css('scrollTop', $(document).height());
 }
