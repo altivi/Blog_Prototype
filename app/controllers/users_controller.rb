1f63d2bd28e5b722074e6ae17fda8d4ca7496324
Altivi <altivi.prog@gmail.com>
2016-02-20 19:57:17 +0200

    All is okay (

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
 	def create
 		@user = User.new(user_params)
 		if @user.save
+			debugger
 			session[:user_id] = @user.id
 			redirect_to root_url
 		else
@@ -17,7 +18,7 @@ class UsersController < ApplicationController
 	private
 
 	def user_params
-		params.require(:user).permit(:name, :email, :password, :password_confirmation)
+		params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
 	end
 
 end
